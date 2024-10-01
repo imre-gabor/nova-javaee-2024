9e23be3ed262959822a6c0a05c599198a07a3248
imre-gabor <gabor@aut.bme.hu>
2024-10-01 12:04:16 +0200

    generate entities from tables

---
+++ bank/src/main/java/bank/model/Account.java
@@ -0,0 +1,91 @@
+package bank.model;
+
+import java.util.Date;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+/**
+ * The persistent class for the account database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Account.findAll", query="SELECT a FROM Account a")
+public class Account  {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer accountid;
+
+	private double balance;
+
+	@Temporal(TemporalType.DATE)
+	private Date createdate;
+
+	//bi-directional many-to-one association to Client
+	@ManyToOne
+	@JoinColumn(name="clientid")
+	private Client client;
+
+	public Account() {
+	}
+
+	public Integer getAccountid() {
+		return this.accountid;
+	}
+
+	public void setAccountid(Integer accountid) {
+		this.accountid = accountid;
+	}
+
+	public double getBalance() {
+		return this.balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public Date getCreatedate() {
+		return this.createdate;
+	}
+
+	public void setCreatedate(Date createdate) {
+		this.createdate = createdate;
+	}
+
+	public Client getClient() {
+		return this.client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(accountid);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		return Objects.equals(accountid, other.accountid);
+	}
+	
+}
---
+++ bank/src/main/java/bank/model/Client.java
@@ -0,0 +1,100 @@
+package bank.model;
+
+import java.util.Objects;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+
+
+/**
+ * The persistent class for the client database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Client.findAll", query="SELECT c FROM Client c")
+public class Client  {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer clientid;
+
+	private String address;
+
+	private String name;
+
+	//bi-directional many-to-one association to Account
+	@OneToMany(mappedBy="client")
+	private Set<Account> accounts;
+
+	public Client() {
+	}
+
+	public Integer getClientid() {
+		return this.clientid;
+	}
+
+	public void setClientid(Integer clientid) {
+		this.clientid = clientid;
+	}
+
+	public String getAddress() {
+		return this.address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Account> getAccounts() {
+		return this.accounts;
+	}
+
+	public void setAccounts(Set<Account> accounts) {
+		this.accounts = accounts;
+	}
+
+	public Account addAccount(Account account) {
+		getAccounts().add(account);
+		account.setClient(this);
+
+		return account;
+	}
+
+	public Account removeAccount(Account account) {
+		getAccounts().remove(account);
+		account.setClient(null);
+
+		return account;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(clientid);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		return Objects.equals(clientid, other.clientid);
+	}
+
+}
